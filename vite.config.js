bc4c5bcf19549616e0d56b8f6c124963958d885c
Elmanda1 <falihelmanda@gmail.com>
2025-07-17 10:18:54 +0700

    fix: add firebase

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Mahesa/', // Tetap perlu untuk assets (CSS, JS, images)
+  base: '/Mahesa/', // Ganti dengan nama repository Anda
   build: {
     outDir: 'dist',
-    sourcemap: false
+    assetsDir: 'assets',
   }
 })
